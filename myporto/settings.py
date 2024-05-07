b05efd8d9a23410993ef7d6a857c2be7bc3c8086
hermantoXYZ <andiupdates@gmail.com>
2024-05-07 09:42:03 +0700

    ip

--- myporto/settings.py
+++ myporto/settings.py
@@ -75,24 +75,24 @@ WSGI_APPLICATION = 'myporto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         # 'NAME': BASE_DIR / 'db.sqlite3',
-#         'NAME': BASE_DIR + '/db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'uHfSmXaQawzCZPsmtgWfyhPJmNQKGgkv',
-        'HOST': 'monorail.proxy.rlwy.net',
-        'PORT': '16147',
+        'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR + '/db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'uHfSmXaQawzCZPsmtgWfyhPJmNQKGgkv',
+#         'HOST': 'monorail.proxy.rlwy.net',
+#         'PORT': '16147',
+#     }
+# }
 
 
 
---
+++ static/scripts.js
@@ -0,0 +1,10 @@
+function toggleMobileMenu() {
+    var mobileMenu = document.getElementById("mobileMenu");
+    mobileMenu.classList.toggle("active");
+}
+
+
+function closeMobileMenu() {
+    var mobileMenu = document.getElementById("mobileMenu");
+    mobileMenu.classList.remove("active");
+}
